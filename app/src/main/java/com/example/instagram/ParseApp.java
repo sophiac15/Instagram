365085f063a29710e534fff2e7468517d5a17263
sophiac15 <sophiaclark@college.harvard.edu>
2019-07-08 17:52:43 -0700

    Initial commit

---
+++ app/src/main/java/com/example/instagram/ParseApp.java
@@ -0,0 +1,25 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.example.instagram.model.Post;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
+        final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
+                .applicationId("myAppId")
+                .clientKey("myMasterKey")
+                .server("http://instagrayum.herokuapp.com/parse")
+                .build();
+
+        Parse.initialize(configuration);
+    }
+}
